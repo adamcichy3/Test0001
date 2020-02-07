ff4e53ec8d28877a4ec281d9ee42879c18c35a36
jonek <adam.cichyy3@gmail.com>
2020-02-07 19:00:29 +0100

    Added whole project

---
+++ ArkanoidGame2020/Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 5f;
+    [SerializeField] float yPush = 10f;
+    bool hasStarted = false;
+    Vector2 paddleToBallVector;
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position; //offset
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBall();
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody>().velocity = new Vector2(xPush, yPush);
+        }
+      
+    }
+
+    private void LockBall()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+}
---
+++ ArkanoidGame2020/Assets/Scripts/Paddle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenSize = 16f;
+    [SerializeField] float min = 1f;
+    [SerializeField] float max = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosition = Input.mousePosition.x / Screen.width *screenSize;
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosition, min, max);
+        transform.position = paddlePos;
+
+    }
+}
